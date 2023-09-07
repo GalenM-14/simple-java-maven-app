213c8b1486ab1b2dad38cfe7ec53ada84f8a5ddd
GalenM-14 <106937823+GalenM-14@users.noreply.github.com>
2023-09-07 16:29:47 -0500

    remove some '

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,13 +11,13 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 #echo 'The following complex command extracts the value of the <name/> element'
 #echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 #set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]" | sed 's/[^a-zA-Z0-9.-]//g``
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]" | sed s/[^a-zA-Z0-9.-]//g`
 #set +x
 
 #echo 'The following complex command behaves similarly to the previous one but'
 #echo 'extracts the value of the <version/> element within <project/> instead.'
 #set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]" | sed 's/[^a-zA-Z0-9.-]//g``
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]" | sed s/[^a-zA-Z0-9.-]//g`
 #set +x
 
 #ENDING="1.0-SNAPSHOT"
